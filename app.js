32c3e3f215b51b3a209d0d15706ebba6320ad2b0
haryanapnx <hary.vai@gmail.com>
2018-04-02 20:59:46 +0700

    changes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 const prog = require('caporal');
+const request = require('request');
+const cheerio = require('cheerio');
 //#1 String Transformation 
 prog //(lowercase)
   .command('lowercase', 'lowercase kata')
@@ -26,8 +28,8 @@ prog //capitalize
 .action(function(args) {
   let ar = args['kata'];
   let capital = ar.split(' ');
-  var a = []
-  for (var i = 0; i < capital.length; i++) {
+  let a = []
+  for (let i = 0; i < capital.length; i++) {
       a[i] = capital[i] = capital[i].charAt(0).toUpperCase() + capital[i].slice(1).toLowerCase(); 
   }
   console.log(a.join(' '));
@@ -84,10 +86,10 @@ prog
 .argument('<string>', 'string')
 .action(function(args){
 
-    var re = args['string'].toLowerCase();
-    var rev = '';
+    let re = args['string'].toLowerCase();
+    let rev = '';
 
-    var len = re.length;
+    let len = re.length;
     let imax = len -1;
     while (len--) {
       rev += re[imax];
@@ -172,13 +174,11 @@ prog // #7. get ip Addres eksternal
 prog // #8. Headlines/scrapingS
 .command('headlines', 'headlines')
 .action(function(args,options,logger) {
-  const request = require('request');
-  const cheerio = require('cheerio');
-  var url = 'https://www.kompas.com/';
+  let url = 'https://www.kompas.com/';
 
   request(url, function (err, res, html) {
     if (!err && res.statusCode == 200) {
-      var $ = cheerio.load(html);
+      let $ = cheerio.load(html);
       $('a.article__link').each(function(i, value) {
         process.stdout.write('Title: '+$(this).text()+'\n')
         process.stdout.write('URL: '+$(this).attr('href')+'\n')
@@ -195,18 +195,18 @@ prog //#9. convert file xls csv
 .argument('<result>','result convert file')
 .action(function(args,options,logger) {
 
-        var xlsx = require('node-xlsx');
-        var fs = require('fs');
+        let xlsx = require('node-xlsx');
+        let fs = require('fs');
         let obj = xlsx.parse(__dirname + `/${args.before}`); // parses a file
         let rows = [];
         let writeStr = "";
-        for(var i = 0; i < obj.length; i++){
-            var sheet = obj[i];
-                        for(var j = 0; j < sheet['data'].length; j++){   //looping row         
+        for(let i = 0; i < obj.length; i++){
+            let sheet = obj[i];
+                        for(let j = 0; j < sheet['data'].length; j++){   //looping row         
             rows.push(sheet['data'][j]);//add the row to the rows array
             }
         }
-        for(var i = 0; i < rows.length; i++) writeStr += rows[i].join(",") + "\n";//creates the csv string to write it to a file
+        for(let i = 0; i < rows.length; i++) writeStr += rows[i].join(",") + "\n";//creates the csv string to write it to a file
         //writes to a file, but you will presumably send the csv as a      
         //response instead
         fs.writeFile(__dirname + `/${args.after}`, writeStr, function(err) {
@@ -226,7 +226,7 @@ prog //#10 screenshot from url
     const fs = require('fs')
     const webshot = require('webshot')
     //filename
-    var option = {
+    let option = {
         shotSize: {
         width: 'all'
       , height: 'all'
@@ -247,16 +247,11 @@ prog //#10 screenshot from url
       });
       }
     }
-
   let numberOfFiles = 1
-  fs.readdirSync('./').forEach(num=>{
-    if(num.includes('screenshot')) numberOfFiles++
-  })
-
+  fs.readdirSync('./').forEach(num=>{if(num.includes('screenshot')) numberOfFiles++})
   if (fs.existsSync(dir)) callScreenshot(args.url,numberOfFiles,options.format)
   else callScreenshot(args.url,1,options.format)
-
-    });
+  });
 
 prog // #11. screenshot from list file
     .command('screenshot-list','ss from list.txt command')
@@ -267,31 +262,46 @@ prog // #11. screenshot from list file
         const webshot = require('webshot')
         let file = fs.readFileSync(`./${args.url}`,'utf8')
             file = file.split('\n')
-        file.map(num=>{fullpath = num.split('/').pop()
-            webshot(num,`${fullpath}.${options.format}`,err=>{
+          file.map(num=>{fullpath = num.split('/').pop()
+              webshot(num,`${fullpath}.${options.format}`,err=>{
                 if(err) console.log(err)
                 else console.log(`from ${num} success`)
             })  
         })
     })
 
-// prog // #12. Headlines/scrapingS
-// .command('movie', 'headlines')
-// .action(function(args,options,logger) {
-//   const request = require('request');
-//   const cheerio = require('cheerio');
-//   var url = 'http://www.21cineplex.com/nowplaying/';
+prog // #12. Headlines/scrapingS
+.command('movies', 'find new film and now playing')
+.action(function(args,options,logger) {
+  const request = require('request');
+  const cheerio = require('cheerio');
+  let url = `https://www.cgv.id/en/movies/now_playing`;
 
-//   request(url, function (err, res, html) {
-//     if (!err && res.statusCode == 200) {
-//       var $ = cheerio.load(html);
-//       $('div.box-right').each(function(i, value) {
-//         process.stdout.write('Title: '+$(this).text()+'\n')
-//         process.stdout.write('URL: '+$(this).attr('p')+'\n')
-//         process.stdout.write('\n');
-//         process.stdout.write('------------------------------------------------------------------------------ \n');
-//     });
-//   }
-// });
-// })
+  request(url, function (err, res, body) {
+    if (!err && res.statusCode == 200) {
+      let $ = cheerio.load(body);
+      $('.movie-list-body a').each(function(i,e) {
+        let film = $(this).attr('href')
+        let uri  = `https://www.cgv.id/${film}`;
+        request(uri, function (err, res, htm) {
+          if (!err && res.statusCode == 200) {
+            let $ = cheerio.load(htm);
+            $('.synopsis-section').each(function(i,e) {
+              
+              let title = $(this).prev().text().trim();
+              let detail  = $(this).children().children().text().split('\t').filter(x => x!='');
+              let synopsis = $(this).children().next().text().trim();
+              process.stdout.write('\nTitle : '+ title);
+              process.stdout.write (detail.join('\n'));
+              process.stdout.write('\nSYNOPSIS : \n\n'+ synopsis);
+              process.stdout.write('\n');
+              process.stdout.write('--------------------------------------------------------------------------------------- \n');
+           });
+          } 
+        });
+      });
+    } 
+  });
+})
 prog.parse(process.argv);
+// body > div.main-container > div > div.main-body-container.skin > div > div.movie-list-body
--- try.js
+++ try.js
@@ -47,54 +47,148 @@
 
 
 
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app     = express();
+// const fetch = require('node-fetch');
+// const cheerio = require('cheerio');
 
-app.get('/scrape', function(req, res){
+// const searchUrl = 'https://www.imdb.com/find?s=tt&ttype=ft&ref_=fn_ft&q=';
+// const movieUrl = 'https://www.imdb.com/title/';
 
-url = 'http://www.imdb.com/title/tt1229340/';
+// const searchCache = {};
+// const movieCache = {};
 
-request(url, function(error, response, html){
-    if(!error){
-        var $ = cheerio.load(html);
+// function searchMovies(searchTerm) {
+//   if(searchCache[searchTerm]) {
+//     console.log('Serving from cache:', searchTerm);
+//     return Promise.resolve(searchCache[searchTerm]);
+//   }
 
-    var title, release, rating;
-    var json = { title : "", release : "", rating : ""};
+//   return fetch(`${searchUrl}${searchTerm}`)
+//     .then(response => response.text())
+//     .then(body => {
+//       const movies = [];
+//       const $ = cheerio.load(body);
+//       $('.findResult').each(function(i, element) {
+//         const $element = $(element);
+//         const $image = $element.find('td a img');
+//         const $title = $element.find('td.result_text a');
 
-    $('.itemprop=name').filter(function(){
-        var data = $(this);
-        title = data.children().first().text();            
-        release = data.children().last().children().text();
+//         const imdbID = $title.attr('href').match(/title\/(.*)\//)[1];
 
-        json.title = title;
-        json.release = release;
-    })
+//         const movie = {
+//           image: $image.attr('src'),
+//           title: $title.text(),
+//           imdbID
+//         };
+//         movies.push(movie);
+//       });
 
-    $('.star-box-giga-star').filter(function(){
-        var data = $(this);
-        rating = data.text();
+//       searchCache[searchTerm] = movies;
 
-        json.rating = rating;
-    })
-}
+//       return movies;
+//     });
+// }
 
+// function getMovie(imdbID) {
+//   if(movieCache[imdbID]) {
+//     console.log('Serving from cache:', imdbID);
+//     return Promise.resolve(movieCache[imdbID]);
+//   }
+
+//   return fetch(`${movieUrl}${imdbID}`)
+//     .then(response => response.text())
+//     .then(body => {
+//       const $ = cheerio.load(body);
+//       const $title = $('.title_wrapper h1');
+
+//       const title = $title.first().contents().filter(function() {
+//         return this.type === 'text';
+//       }).text().trim();
+//       const rating = $('meta[itemProp="contentRating"]').attr('content');
+//       const runTime = $('time[itemProp="duration"]').first().contents().filter(function() {
+//         return this.type === 'text';
+//       }).text().trim();
+
+//       const genres = [];
+//       $('span[itemProp="genre"]').each(function(i, element) {
+//         const genre = $(element).text();
+//         genres.push(genre);
+//       });
+
+//       const datePublished = $('meta[itemProp="datePublished"]').attr('content');
+//       const imdbRating = $('span[itemProp="ratingValue"]').text();
+//       const poster = $('div.poster a img').attr('src');
+//       const summary = $('div.summary_text').text().trim();
+
+
+//       function getItems(itemArray) {
+//         return function(i, element) {
+//           const item = $(element).text().trim();
+//           itemArray.push(item);
+//         };
+//       }
+
+//       const directors = [];
+//       $('span[itemProp="director"]').each(getItems(directors));
+
+//       const writers = [];
+//       $('.credit_summary_item span[itemProp="creator"]').each(getItems(writers));
+
+//       const stars = [];
+//       $('.credit_summary_item span[itemProp="actors"]').each(getItems(stars));
+
+//       const storyLine = $('#titleStoryLine div[itemProp="description"] p').text().trim();
+
+//       const companies = [];
+//       $('span[itemType="http://schema.org/Organization"]').each(getItems(companies));
+
+//       const trailer = $('a[itemProp="trailer"]').attr('href');
+
+//       const movie = {
+//         imdbID,
+//         title,
+//         rating,
+//         runTime,
+//         genres,
+//         datePublished,
+//         imdbRating,
+//         poster,
+//         summary,
+//         directors,
+//         writers,
+//         stars,
+//         storyLine,
+//         companies,
+//         trailer: `https://www.imdb.com${trailer}`
+//       };
+
+//       movieCache[imdbID] = movie;
+
+//       return movie;
+//     });
+// }
+
+// module.exports = {
+//   searchMovies,
+//   getMovie
+// };
 // To write to the system we will use the built in 'fs' library.
 // In this example we will pass 3 parameters to the writeFile function
 // Parameter 1 :  output.json - this is what the created filename will be called
 // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
 // Parameter 3 :  callback function - a callback function to let us know the status of our function
 
-fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+// fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
 
-    console.log('File successfully written! - Check your project directory for the output.json file');
+//     console.log('File successfully written! - Check your project directory for the output.json file');
 
-})
+// })
 
-// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-res.send('Check your console!')
+// // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+// res.send('Check your console!')
 
-    }) ;
-})
+//     }) ;
+// })
+
+var movies = $this.attr('href')
+        let uri = `https://www.cgv.id/${movies}`
+         request(uri, function (err, res, body) {})
